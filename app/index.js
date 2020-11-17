d6b3b2ab119275f3695f1aaba528e7304c83ffc3
benoit barges <benoitbarges@gmail.com>
2020-11-17 15:57:24 +0100

    refacto index hoosk

--- app/index.js
+++ app/index.js
@@ -45,47 +45,6 @@ function App () {
   )
 }
 
-// class App extends React.Component {
-//   state = {
-//     theme: 'light',
-//     toggleTheme: () => {
-//       this.setState(({ theme }) => ({
-//         theme: theme === 'light' ? 'dark' : 'light'
-//       }))
-//     }
-//   }
-//   render() {
-//     return (
-//       <Router>
-//         <ThemeProvider value={this.state}>
-//           <div className={this.state.theme}>
-//             <div className='container'>
-//               <Nav />
-
-//               <React.Suspense fallback={<Loading />}>
-//                 <Switch>
-//                   <Route
-//                     exact
-//                     path='/'
-//                     render={() => <Posts type='top' />}
-//                   />
-//                   <Route
-//                     path='/new'
-//                     render={() => <Posts type='new' />}
-//                   />
-//                   <Route path='/post' component={Post} />
-//                   <Route path='/user' component={User} />
-//                   <Route render={() => <h1>404</h1>} />
-//                 </Switch>
-//               </React.Suspense>
-//             </div>
-//           </div>
-//         </ThemeProvider>
-//       </Router>
-//     )
-//   }
-// }
-
 ReactDOM.render(
   <App />,
   document.getElementById('app')

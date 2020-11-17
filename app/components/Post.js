418310a48a91f2dcb7834931926e6d53e9be5511
benoit barges <benoitbarges@gmail.com>
2020-11-17 16:40:10 +0100

    loading hooks

--- app/components/Post.js
+++ app/components/Post.js
@@ -6,67 +6,79 @@ import PostMetaInfo from './PostMetaInfo'
 import Title from './Title'
 import Comment from './Comment'
 
-export default class Post extends React.Component {
-  state = {
-    post: null,
-    loadingPost: true,
-    comments: null,
-    loadingComments: true,
-    error: null,
+const postReducer = (state, action) => {
+  if (action.type === 'fetchPost') {
+    return {
+      ...state,
+      post: action.post,
+      loadingPost: false
+    }
+  } else if (action.type === 'fetchComments') {
+    return  {
+      ...state,
+      loadingComments: false,
+      comments: action.comments
+    }
+  } else if (action.type === 'error') {
+    return {
+      ...state,
+      error: action.error.message
+    }
+  } else {
+    throw new Error("This action type isn't supported.")
   }
-  componentDidMount() {
-    const { id } = queryString.parse(this.props.location.search)
+}
 
+export default function Post ({ location }) {
+  const { id } = queryString.parse(location.search)
+  const [state, dispatch] = React.useReducer(
+    postReducer,
+    { post: null, loadingPost: true, comments: null, loadingComments: true, error: null}
+  )
+
+  React.useEffect(() => {
     fetchItem(id)
       .then((post) => {
-        this.setState({ post, loadingPost: false })
+        dispatch({ type: 'fetchPost', post })
 
         return fetchComments(post.kids || [])
       })
-      .then((comments) => this.setState({
-        comments,
-        loadingComments: false
-      }))
-      .catch(({ message }) => this.setState({
-        error: message,
-        loadingPost: false,
-        loadingComments: false
-      }))
+      .then(comments => dispatch({ type: 'fetchComments', comments }))
+      .catch(error =>dispatch({ type: 'error', error }))
+  }, [id])
+
+  const { post, loadingPost, comments, loadingComments, error } = state
+
+  if (error) {
+    return <p className='center-text error'>{error}</p>
   }
-  render() {
-    const { post, loadingPost, comments, loadingComments, error } = this.state
 
-    if (error) {
-      return <p className='center-text error'>{error}</p>
-    }
-
-    return (
-      <React.Fragment>
-        {loadingPost === true
-          ? <Loading text='Fetching post' />
-          : <React.Fragment>
-              <h1 className='header'>
-                <Title url={post.url} title={post.title} id={post.id} />
-              </h1>
-              <PostMetaInfo
-                by={post.by}
-                time={post.time}
-                id={post.id}
-                descendants={post.descendants}
+  return (
+    <React.Fragment>
+      {loadingPost === true
+        ? <Loading text='Fetching post' />
+        : <React.Fragment>
+            <h1 className='header'>
+              <Title url={post.url} title={post.title} id={post.id} />
+            </h1>
+            <PostMetaInfo
+              by={post.by}
+              time={post.time}
+              id={post.id}
+              descendants={post.descendants}
+            />
+            <p dangerouslySetInnerHTML={{__html: post.text}} />
+          </React.Fragment>}
+      {loadingComments === true
+        ? loadingPost === false && <Loading text='Fetching comments' />
+        : <React.Fragment>
+            {comments.map((comment) =>
+              <Comment
+                key={comment.id}
+                comment={comment}
               />
-              <p dangerouslySetInnerHTML={{__html: post.text}} />
-            </React.Fragment>}
-        {loadingComments === true
-          ? loadingPost === false && <Loading text='Fetching comments' />
-          : <React.Fragment>
-              {comments.map((comment) =>
-                <Comment
-                  key={comment.id}
-                  comment={comment}
-                />
-              )}
-            </React.Fragment>}
-      </React.Fragment>
-    )
-  }
+            )}
+          </React.Fragment>}
+    </React.Fragment>
+  )
 }

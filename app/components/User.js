5d021b2655fdc465e6ce714b20829af9ab83f02f
benoit barges <benoitbarges@gmail.com>
2020-11-19 10:07:01 +0100

    finished with hooks

--- app/components/User.js
+++ app/components/User.js
@@ -5,60 +5,74 @@ import Loading from './Loading'
 import { formatDate } from '../utils/helpers'
 import PostsList from './PostsList'
 
-export default class User extends React.Component {
-  state = {
-    user: null,
-    loadingUser: true,
-    posts: null,
-    loadingPosts: true,
-    error: null,
+const userReducer = (state, action) => {
+  if (action.type === 'fetchUser') {
+    return {
+      ...state,
+      user: action.user,
+      loadingUser: false,
+      error: null
+    }
+  } else if (action.type === 'fetchPosts') {
+    return {
+      ...state,
+      posts: action.posts,
+      loadingPosts: false,
+      error: null
+    }
+  } else if (action.type === 'error') {
+    return {
+      ...state,
+      error: action.error.message
+    }
+  } else {
+    throw new Error("This action type isn't supported.")
   }
-  componentDidMount() {
-    const { id } = queryString.parse(this.props.location.search)
+}
 
+export default function User ({ location }) {
+  const [state, dispatch] = React.useReducer(
+    userReducer,
+    { user: null, loadingUser: true, posts: null, loadingPosts: true, error: null }
+  )
+
+  React.useEffect(() => {
     fetchUser(id)
       .then((user) => {
-        this.setState({ user, loadingUser: false})
+        dispatch({ type: 'fetchUser', user })
 
         return fetchPosts(user.submitted.slice(0, 30))
       })
-      .then((posts) => this.setState({
-        posts,
-        loadingPosts: false,
-        error: null
-      }))
-      .catch(({ message }) => this.setState({
-        error: message,
-        loadingUser: false,
-        loadingPosts: false
-      }))
-  }
-  render() {
-    const { user, posts, loadingUser, loadingPosts, error } = this.state
+      .then(posts => dispatch({ type: 'fetchPosts', posts }))
+      .catch(error => dispatch({ type: 'error', error }))
+  }, [id])
 
-    if (error) {
-      return <p className='center-text error'>{error}</p>
-    }
+  const { id } = queryString.parse(location.search)
 
-    return (
-      <React.Fragment>
-        {loadingUser === true
-          ? <Loading text='Fetching User' />
-          : <React.Fragment>
-              <h1 className='header'>{user.id}</h1>
-              <div className='meta-info-light'>
-                <span>joined <b>{formatDate(user.created)}</b></span>
-                <span>has <b>{user.karma.toLocaleString()}</b> karma</span>
-              </div>
-              <p dangerouslySetInnerHTML={{__html: user.about}} />
-            </React.Fragment>}
-        {loadingPosts === true
-          ? loadingUser === false && <Loading text='Fetching posts'/>
-          : <React.Fragment>
-              <h2>Posts</h2>
-              <PostsList posts={posts} />
-            </React.Fragment>}
-      </React.Fragment>
-    )
+  const { user, posts, loadingUser, loadingPosts, error } = state
+
+  if (error) {
+    return <p className='center-text error'>{error}</p>
   }
+
+  return (
+    <React.Fragment>
+      {loadingUser === true
+        ? <Loading text='Fetching User' />
+        : <React.Fragment>
+            <h1 className='header'>{user.id}</h1>
+            <div className='meta-info-light'>
+              <span>joined <b>{formatDate(user.created)}</b></span>
+              <span>has <b>{user.karma.toLocaleString()}</b> karma</span>
+            </div>
+            <p dangerouslySetInnerHTML={{__html: user.about}} />
+          </React.Fragment>}
+      {loadingPosts === true
+        ? loadingUser === false && <Loading text='Fetching posts'/>
+        : <React.Fragment>
+            <h2>Posts</h2>
+            <PostsList posts={posts} />
+          </React.Fragment>}
+    </React.Fragment>
+  )
 }
